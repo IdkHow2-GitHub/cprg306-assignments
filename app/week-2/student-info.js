b26945600cbf540be48f2f2cd0cced07642d9aa4
IdkHow2 <benjamin.simms@edu.sait.ca>
2024-09-09 14:04:42 -0600

    Week 2
    - added week 2 page
    - changed the root page
    - idk what else

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Benjamin Simms</p>
+            <p>https://github.com/IdkHow2-GitHub/cprg306-assignments</p>
+        </main>
+    );
+}
